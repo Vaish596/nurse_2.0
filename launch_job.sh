2ccc14bffe758fa131cedbbd8fc6e4f4a52f58c8
Vaish596 <88754337+Vaish596@users.noreply.github.com>
2026-02-06 10:54:08 +0100

    Update launch_job.sh for session and video file args

--- launch_job.sh
+++ launch_job.sh
@@ -1,13 +1,19 @@
-#512
+#!/bin/bash
+
+SESSION_ID=$1
+VIDEO_FILE=$2
+
+echo "$SESSION_ID" "$VIDEO_FILE"
+
 srun \
-  --cpus-per-task=9 \
-  --job-name='nurse_385' \
+  --cpus-per-task=8 \
+  --job-name="nurse_${SESSION_ID}" \
   --gpus=0 \
-  --mem="40GB" \
+  --mem="128GB" \
   --partition=batch,H100-RP,A100-80GB,H200,A100-40GB,RTXA6000,V100-32GB \
   --ntasks=1 \
-  --container-mounts="/ds":"/ds","/netscratch":"/netscratch","`pwd`":"`pwd`"  \
+  --container-mounts="/ds":"/ds","/netscratch":"/netscratch","`pwd`":"`pwd`" \
   --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_23.04-py3.sqsh \
   --container-workdir="`pwd`" \
   --time=01-00:00 \
-  bash job.sh 
+  bash job.sh "$SESSION_ID" "$VIDEO_FILE"
