f10c2939bd3047cee94704920fec7591e117b0e7
Vaish596 <88754337+Vaish596@users.noreply.github.com>
2026-02-06 10:53:42 +0100

    update script execution
    Updated job.sh to comment out deface commands and modify Python script call.

--- job.sh
+++ job.sh
@@ -1,4 +1,12 @@
+#comment everything and run deface command to deface any video
 sh install.sh
-# deface -t 0.6 384_all_cams.mkv
-python cluster_int_sensor_ani.py
+
+SESSION_ID=$1
+VIDEO_FILE=$2
+# deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/10383_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_1/10383_all_cams.mp4
+# python cluster_int_sensor_ani_new.py
+
+python cluster_int_sensor_ani_new.py \
+  --session_id "$SESSION_ID" \
+  --video_file "$VIDEO_FILE"
 # echo 'Done generating'
