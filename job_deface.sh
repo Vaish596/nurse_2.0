6abd12a69c89a7139d65e1fc7eb27bedbfc8c16f
Vaish596 <88754337+Vaish596@users.noreply.github.com>
2026-02-26 10:27:47 +0100

    Add deface commands for multiple video files
    Added commands to deface multiple video files with specified output paths.

---
+++ job_deface.sh
@@ -0,0 +1,13 @@
+sh install.sh
+
+# SESSION_ID=$1
+# VIDEO_FILE=$2
+# deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/"${SESSION_ID}"_all_cams_anonymized.mp4 "${VIDEO_FILE}" #/ds/videos/nurse_2.0/videos_sync/1080p/session_2/389_all_cams.mp4
+deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/601_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_2/601_all_cams.mp4
+deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/602_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_2/602_all_cams.mp4
+deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/603_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_2/603_all_cams.mp4
+deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/604_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_2/604_all_cams.mp4
+deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/605_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_2/605_all_cams.mp4
+deface -t 0.6 -o /netscratch/shirbhata/nurse_2.0/deface/606_all_cams_anonymized.mp4 /ds/videos/nurse_2.0/videos_sync/1080p/session_2/606_all_cams.mp4
+
+# deface -t 0.6 -o <output_file_path> <input file path>
